acba0bee4233133df66bc8d0bfaff34060f05fe2
michaelcandido <mcandido12@gmail.com>
2020-06-04 10:55:02 -0700

    add routes

--- application.py
+++ application.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/get-feed')
+def get_feed():
+    pass
+
+@app.route('/update-queries', methods=['POST'])
+def update_queries():
+    print('test message')
+    print(request.data)
+    pass
