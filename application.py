02a95cf133c86cc52688c7d0cb8895b1e6eca441
michaelcandido <mcandido12@gmail.com>
2020-06-04 11:32:33 -0700

    airtable integration

--- application.py
+++ application.py
@@ -1,5 +1,12 @@
-from flask import Flask, request
+from airtable import Airtable
+from flask import Flask, request, jsonify
+from werkzeug.exceptions import BadRequest
+
+AIRTABLE_KEY = 'keyIiyOZTJ7kdkOth'
+
 app = Flask(__name__)
+queries_tbl = Airtable(AIRTABLE_KEY, 'Queries')
+feed_tbl = Airtable(AIRTABLE_KEY, 'FeedEntries')
 
 @app.route("/")
 def hello():
@@ -11,6 +18,13 @@ def get_feed():
 
 @app.route('/update-queries', methods=['POST'])
 def update_queries():
-    print('test message')
-    print(request.data)
-    pass
+    data = request.json
+    if 'queries' not in data:
+        raise BadRequest('missing queries property')
+    
+    query_ids = queries_tbl.get_all(fields='Query')
+    queries_tbl.batch_delete(query_ids)
+
+    query_records = [{'Query': q} for q in data['queries']]
+    queries_tbl.batch_insert(query_records)
+    return 'Success'
